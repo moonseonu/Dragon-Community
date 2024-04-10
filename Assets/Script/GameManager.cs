5ad765bfe6f3a1727ec56b903a44767d3185ced6
moonseonu <48229760+moonseonu@users.noreply.github.com>
2024-04-10 17:48:40 +0900

    클라이언트 업로드

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance;
+
+    public bool isMatching = false;
+    private void Awake()
+    {
+        if (instance != null) Destroy(gameObject);
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Networking.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private const string SERVER_IP = "127.0.0.1"; // 서버 IP 주소
+    private const int SERVER_PORT = 9000;         // 서버 포트 번호
+
+    private TcpClient client;
+    private NetworkStream stream;
+    public class Packet
+    {
+        public string playerName = "moon";
+        public int playerID = 1234;
+
+        // Serialize 함수: 패킷을 바이트 배열로 직렬화하는 메서드
+        public byte[] Serialize()
+        {
+            byte[] playerNameBytes = Encoding.UTF8.GetBytes(playerName);
+            byte[] playerIDBytes = BitConverter.GetBytes(playerID);
+
+            // int 변수의 크기를 직접 지정합니다.
+            int intSize = sizeof(int);
+
+            byte[] data = new byte[32 + intSize]; // intSize로 수정합니다.
+            Buffer.BlockCopy(playerNameBytes, 0, data, 0, playerNameBytes.Length);
+            Buffer.BlockCopy(playerIDBytes, 0, data, 32, intSize); // intSize로 수정합니다.
+
+            return data;
+        }
+    }
+    Packet packet = new Packet();
+    
+
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.isMatching)
+        {
+            GameManager.instance.isMatching = false;
+            SendPacket(packet);
+        }
+    }
+
+    void ConnectToServer()
+    {
+        try
+        {
+            client = new TcpClient(SERVER_IP, SERVER_PORT);
+            stream = client.GetStream();
+            Debug.Log("Connected to server");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to connect to server: {e.Message}");
+        }
+    }
+
+    void SendPacket(Packet packet)
+    {
+        try
+        {
+            byte[] data = packet.Serialize();
+            stream.Write(data, 0, data.Length);
+
+            Debug.Log("Packet sent to server");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to send packet to server: {e.Message}");
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (client != null)
+        {
+            client.Close();
+            Debug.Log("Disconnected from server");
+        }
+    }
+}
